984b537f3d95cc9e73b493887b6fd87cad17779b
Matt Taylor <taylor.matt777@gmail.com>
2016-03-05 01:49:44 -0700

    Counting the players right from both teams

--- app/www/js/games/games.controller.js
+++ app/www/js/games/games.controller.js
@@ -8,6 +8,8 @@
 
   function GamesController($scope, GamesService)
   {
+    $scope.players = players;
+
     // Get games from database
     GamesService.getGames().then(
     function(response)
@@ -19,5 +21,10 @@
         game.endMoment= moment.unix(game.endTime);
       });
     });
+
+    function players(game)
+    {
+      return game.blueTeam.length + game.orangeTeam.length;
+    }
   }
 })();
