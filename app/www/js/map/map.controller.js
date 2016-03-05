4acce93f05490c0b50484cde215bda9fffb97315
Conner Dodge <connerdodge@u.boisestate.edu>
2016-03-04 21:58:25 -0700

    It works. WOW

--- app/www/js/map/map.controller.js
+++ app/www/js/map/map.controller.js
@@ -8,12 +8,10 @@
 
   function MapController($scope, $ionicLoading)
   {
-    google.maps.event.addDomListener(window, 'load', function() 
-    {
       var myLatlng, myLocation, mapOptions, map, watchOptions;
 
       mapOptions = {
-          center: myLatlng,
+          center: new google.maps.LatLng(43.618710, -116.214607),
           zoom: 16,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           mapTypeControl: false,
@@ -21,34 +19,36 @@
           zoomControl: false
       };
 
-      // watchOptions = {
-      //   enableHighAccuracy: false,
-      //   timeout: 5000,
-      //   maximumAge: 0
-      // };
+      watchOptions = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+      };
 
       map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-      navigator.geolocation.getCurrentPosition(function(pos) {
-          map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-          myLocation = new google.maps.Marker({
-              position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-              map: map,
-              title: "Current Location"
-          });
-      });
+      navigator.geolocation.getCurrentPosition(currentPositionSuccess);
 
-      // function watchSuccess(pos) {
-      //   myLocation = pos.coords;
-      // }
+      function currentPositionSuccess(pos) 
+      {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        myLocation = new google.maps.Marker({
+            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+            map: map,
+            title: "Current Location"
+        });
+      }
 
-      // function watchError(err) {
-      //   console.warn('ERROR(' + err.code + '): ' + err.message);
-      // }
+      function watchSuccess(pos) {
+        myLocation = pos.coords;
+      }
 
-      //navigator.geolocation.watchPosition(watchSuccess, watchError, watchOptions);
+      function watchError(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+      }
+
+      navigator.geolocation.watchPosition(watchSuccess, watchError, watchOptions);
 
       $scope.map = map;
-    });
   };
 })();
