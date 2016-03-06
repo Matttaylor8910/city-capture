43d4768c0076d3b876fc056eab20ff59d63a67e6
Roger Ogden <rogerogden@u.boisestate.edu>
2016-03-05 21:42:45 -0700

    Changed wording around a bit. Deleted joining halfway, because we aren't doing that.

--- app/www/js/about/about.constant.js
+++ app/www/js/about/about.constant.js
@@ -5,11 +5,11 @@
     .constant('FAQs', [
       {
         question: 'What is the objective of the game?',
-        answer: 'The objective of the game is to capture more flags on the map than the other team.'
+        answer: 'The objective of City Capture is to capture more flags on the map than the other team within the time limit.'
       },
       {
         question: 'How do I capture flags?',
-        answer: 'First, locate a flag on the map that you want to capture and start moving towards it. Once you are within 10 meters of the flag, you will automatically begin capturing it as long as you have your mobile device with you.'
+        answer: 'First, locate a flag on the map that you want to capture and start moving towards it. Once you are within 10 meters of the flag, you will automatically begin capturing it as long as you have your mobile device with you and the app running.'
       },
       {
         question: 'What if the other team has already captured a flag?',
@@ -17,15 +17,11 @@
       },
       {
         question: 'What are points?',
-        answer: 'Points specify how long a team was at a location. Every 1 second, you gain 1 point for every member of your team that is at the location.'
+        answer: 'Points specify how long a team was at a location. Every 1 second, you gain 1 point for every member of your team that is at the location. That is, if you have 3 team members at a flag location, you will gain three points per second.'
       },
       {
         question: 'How long are the games?',
-        answer: 'Games can be between 1 and 6 hours. The 1 hour games are meant to be more competitive, while the 6 hour games are more leisurely.'
-      },
-      {
-        question: 'When can I join a game?',
-        answer: 'Games can be joined anytime until the halfway point. After the halfway point, you are unable to join that game.'
+        answer: 'Games can be between 1 and 6 hours. The 1 hour games are meant to be more competitive, with fast-pace action, while the 6 hour games are more leisurely and allow you to explore the places that harbor the flag.'
       }
     ]);
 
